2388f23adca45355938c001aba127c7f3706fbb8
wild-mental <wild.mental@kidsnote.com>
2024-01-30 14:30:40 +0900

    user1 모듈1 개발  - 모듈1 클래스 추가  - 공통부분 클래스 수정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // sample code for initial commit
+        // 공통 기능이 필요해서 여기다가 작성합니다!
+        // 함께 사용하기에 매우 유용하겠죠?!!??!?
     }
 }
---
+++ src/Module1Class1.java
@@ -0,0 +1,5 @@
+public class Module1Class1 {
+    public static void main(String[] args) {
+        // 모듈 1에 대한 개발
+    }
+}
