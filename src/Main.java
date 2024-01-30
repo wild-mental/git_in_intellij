ee3de6d9a7c480d582d5f32fb74d1d4c44fa169e
wild-mental <wild.mental@kidsnote.com>
2024-01-30 14:33:58 +0900

    user2 모듈2 개발  - 모듈2 클래스 추가  - 공통 부분 수정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // sample code for initial commit
+        // 공통부분을 나도 만들었습니다!
+        // 먼저 만든 사람이 누군지는 상관없습니다!
+        // 왜냐면 내가 필요할 것 같아서 내가 만들었습니다!
     }
 }
